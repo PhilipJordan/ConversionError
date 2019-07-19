e95195fd5c12bcbeb8a3779fdf61511454f291b6
Philip Jordan <phil.jordan@careworkstech.com>
2019-07-19 11:30:36 -0400

    initial commit

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,13 +5,7 @@ require 'watir'
 client = Selenium::WebDriver::Remote::Http::Default.new
 client.read_timeout = 300
 
-
 browser = Watir::Browser.new :chrome, :http_client => client, 'chromeOptions' => {"args" => ["--start-maximized", "--disable-infobars"]}
-
-
-
-
-
 
 Before do
   @browser = browser
